47b45735fdf6e5efda45e0bfab73a72fae666a86
Tiffany <ong.tifffany1503@gmail.com>
2018-10-08 02:31:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 require "pry"
 
 def turn_count(board)
-  counter = 3
+  counter = 0
   board.each do |token|
     if token == "X" || token == "O"
       counter +- 1
@@ -11,5 +11,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board).even? ? "X" : "O"
+  turn_count(board).odd? ? "X" : "O"
 end
